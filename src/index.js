777ff5307a2bb6437643bedc4291cab084935eb9
ISTURIZrp <136546648+ISTURIZrp@users.noreply.github.com>
2025-05-08 14:53:49 -0600

    Actualizar index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // Tailwind importa aquí
+import './style.css'; // Tailwind importa aquí
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
